82dc43d052298315968a0c56b52831dbcaf5a7c2
LorenzoSantamarinaProyecto <Proyectofinalprueba@gmail.com>
2024-10-08 13:39:06 -0300

    git add

--- express-redirect-app/index.js
+++ express-redirect-app/index.js
@@ -3,12 +3,12 @@ const app = express();
 
 // Endpoint 1 - Redirect to the first hardcoded URL
 app.get('/endpoint1', (req, res) => {
-    res.redirect(301, 'https://example.com/your-first-url');
+    res.redirect(301, 'https://www.google.com/search?q=equi+fernandez&rlz=1C1GCEA_enAR1099AR1099&oq=eq&gs_lcrp=EgZjaHJvbWUqDAgAECMYJxiABBiKBTIMCAAQIxgnGIAEGIoFMgoIARAuGLEDGIAEMg8IAhBFGDkYgwEYsQMYgAQyEwgDEC4YgwEYxwEYsQMY0QMYgAQyCggEEAAYsQMYgAQyEAgFEAAYgwEYsQMYgAQYigUyDQgGEAAYgwEYsQMYgAQyBggHEEUYPNIBCDMwMjBqMGo3qAIAsAIA&sourceid=chrome&ie=UTF-8');
 });
 
 // Endpoint 2 - Redirect to the second hardcoded URL
 app.get('/endpoint2', (req, res) => {
-    res.redirect(301, 'https://example.com/your-second-url');
+    res.redirect(301, 'https://www.google.com/search?gs_ssp=eJzj4tVP1zc0zEkyTstISckwYPTiTsxJzFMoSyxKzUkEAHsvCPs&q=alan+varela&rlz=1C1GCEA_enAR1099AR1099&oq=alan+&gs_lcrp=EgZjaHJvbWUqDwgBEC4YQxixAxiABBiKBTIMCAAQIxgnGIAEGIoFMg8IARAuGEMYsQMYgAQYigUyBggCEEUYOTIMCAMQABhDGIAEGIoFMg0IBBAuGNQCGLEDGIAEMg0IBRAuGNQCGLEDGIAEMg0IBhAuGIMBGLEDGIAEMg0IBxAuGIMBGLEDGIAEMg0ICBAAGIMBGLEDGIAEMgoICRAuGLEDGIAE0gEIMjg4M2owajmoAgCwAgA&sourceid=chrome&ie=UTF-8');
 });
 
 // Listen on port 3000, which will be set dynamically by Vercel
